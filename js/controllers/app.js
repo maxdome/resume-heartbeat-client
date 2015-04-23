41dfeb76cd07526995a9e9dc57a245161e2340cc
Christoph Herrmann <developer@dragonprojects.de>
2015-04-23 12:42:37 +0200

    Formular for our use cases to create token, store and get playback positions

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -7,4 +7,27 @@ var dependencies = [];
 
 angular.module(module.exports, dependencies)
     .controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.user = {
+            id: null
+        };
+        $scope.asset = {
+            id: null,
+            playbackPosition: null
+        };
+        $scope.token = {
+            token: null
+        };
+        $scope.token = function () {
+            console.log($scope.user.id);
+            console.log($scope.asset.id);
+            $scope.token.token = 'newToken';
+        };
+        $scope.store = function () {
+            console.log($scope.token.token);
+            console.log($scope.asset.playbackPosition);
+        };
+        $scope.get = function () {
+            console.log($scope.user.id);
+            console.log($scope.asset.id);
+        };
     }]);
